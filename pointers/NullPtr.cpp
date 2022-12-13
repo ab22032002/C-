4852738dad5fde645079eb690be3201446e4b32c
Abhinav Singh Bhardwaj <95396382+ab22032002@users.noreply.github.com>
2022-12-13 10:12:00 +0530

    small configuers

---
+++ pointers/NullPtr.cpp
@@ -0,0 +1,11 @@
+    #include <iostream>
+
+    using namespace std;
+    int main () {
+            int  *ptr = NULL;
+            cout << "The value of ptr is " << ptr ;
+        
+        //    if(ptr)     // succeeds if p is not null
+        //    if(!ptr)    // succeeds if p is null
+    return 0;
+    }
